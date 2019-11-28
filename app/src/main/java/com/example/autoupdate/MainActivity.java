5ef5164cc432fd389bbb87d5411d2e2b25a0c369
Rusydi <rusydi@v-series.net>
2019-11-28 17:57:48 +0800

    Updated 2

--- app/src/main/java/com/example/autoupdate/MainActivity.java
+++ app/src/main/java/com/example/autoupdate/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.github.javiersantos.appupdater.AppUpdater;
 import com.github.javiersantos.appupdater.enums.UpdateFrom;
@@ -21,11 +22,29 @@ public class MainActivity extends AppCompatActivity
                 .setUpdateFrom(UpdateFrom.GITHUB)
                 .setGitHubUserAndRepo("wanrusydi", "autoupdatetest")
                 .start();
+        Test();
     }
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState)
     {
         super.onPostCreate(savedInstanceState);
+    }
+
+    private void Test()
+    {
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
+        Test2();
+    }
+
+    private void Test2()
+    {
+        Toast.makeText(this, "Test2", Toast.LENGTH_SHORT).show();
+        Test3();
+    }
+
+    private void Test3()
+    {
+        Toast.makeText(this, "Test3", Toast.LENGTH_SHORT).show();
     }
 }
