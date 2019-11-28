9883d3d30ce242de08c90c5f289b9ebcfda8421c
Rusydi <rusydi@v-series.net>
2019-11-28 17:54:42 +0800

    Updated

--- app/src/main/java/com/example/autoupdate/MainActivity.java
+++ app/src/main/java/com/example/autoupdate/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.autoupdate;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.enums.UpdateFrom;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +16,16 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AppUpdater(this)
+                .setUpdateFrom(UpdateFrom.GITHUB)
+                .setGitHubUserAndRepo("wanrusydi", "autoupdatetest")
+                .start();
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
     }
 }
